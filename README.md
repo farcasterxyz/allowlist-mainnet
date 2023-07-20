df8da93f2674b4ccab77613dcf8ce3f51f852504
Aditya Kulkarni <adityapk@tr32.home.adityapk.com>
2023-07-20 10:42:50 -0500

    Create the initial allowlist config

---
+++ README.md
@@ -0,0 +1,11 @@
+## Hubble Mainnet Network Config
+
+This repo contains the hubble mainnet's network config. It can be updated to:
+
+1. Add PeerIDs to the mainnet allow list
+2. Remove/Deny PeerIDs from the mainnet allow list
+3. Set a minimum App Version for Hubble
+
+### Usage
+
+Add PeerIDs to `networkConfig.js` as needed. Hubble will re-download the network config every hour.
--- networkConfig.js
+++ networkConfig.js
@@ -1,6 +1,18 @@
-const config = {
-    network: 1,
-    allowList: [],
-    denyList: [],
-    minVersion: "1.3.3"
+/** Network config that will be fetched by each Hub instance */
+config = {
+    // The network that this applies to
+    network: 1, 
+
+    // Additional allowed peerIDs
+    allowedPeers: [
+        "12D3KooWEWMXM8faJFXsVt6RUPBopBVvWUbnmETNWD9Z1Uijr7PH", // @adityapk00 - Hub2
+    ],
+    
+    // Deny these peers, even if they are in the allowList on the Hub. Deny list overrides allowlist
+    deniedPeers: [], 
+
+    // Minimum version of the Hub. 
+    // WARNING: If a Hub is below this version, it will not be able to start. 
+    // A running hub below this version WILL SHUT DOWN!
+    minAppVersion: "1.3.3" 
 };
